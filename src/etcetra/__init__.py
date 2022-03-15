476dc01efd5ccd5bfc5ddb85348329a9219d3d7d
Kyujin Cho <kyujin.cho@lablup.com>
2022-03-15 12:04:43 +0000

    add __all__ on types and __init__ to prevent mypy error

--- src/etcetra/__init__.py
+++ src/etcetra/__init__.py
@@ -1,3 +1,11 @@
+from . import client as _client
+from . import types as _types
+
+__all__ = (
+    *_client.__all__,
+    *_types.__all__,
+)
+
 from .client import *  # noqa
 from .types import *  # noqa
 
