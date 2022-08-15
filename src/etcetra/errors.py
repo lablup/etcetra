f793020d13051c0f6d6009df1d4b7bfaa6cf3368
Kyujin Cho <kyujin.cho@lablup.com>
2022-08-15 15:33:22 +0800

    fix lint error

--- src/etcetra/errors.py
+++ src/etcetra/errors.py
@@ -10,54 +10,69 @@ class EtcetraError(Exception):
     code: ClassVar[int]
     debug_map: Optional[Mapping[str, Any]]
 
-    def __init__(self, message = None, debug_map = None):
+    def __init__(self, message=None, debug_map=None):
         super().__init__(message)
         self.debug_map = debug_map
 
     def __int__(self):
         return self.code
 
+
 class EtcdUnknownError(EtcetraError):
     code = code_pb2.UNKNOWN
+
 
 class EtcdInvalidArgumentError(EtcetraError):
     code = code_pb2.INVALID_ARGUMENT
 
+
 class EtcdDeadlineExceededError(EtcetraError):
     code = code_pb2.DEADLINE_EXCEEDED
+
 
 class EtcdNotFoundError(EtcetraError):
     code = code_pb2.NOT_FOUND
 
+
 class EtcdAlreadyExistsError(EtcetraError):
     code = code_pb2.ALREADY_EXISTS
+
 
 class EtcdPermissionDeniedError(EtcetraError):
     code = code_pb2.PERMISSION_DENIED
 
+
 class EtcdUnauthenticatedError(EtcetraError):
     code = code_pb2.UNAUTHENTICATED
+
 
 class EtcdTooManyRequestError(EtcetraError):
     code = code_pb2.RESOURCE_EXHAUSTED
 
+
 class EtcdBadRequestError(EtcetraError):
     code = code_pb2.FAILED_PRECONDITION
+
 
 class EtcdAbortedError(EtcetraError):
     code = code_pb2.ABORTED
 
+
 class EtcdOutOfRangeError(EtcetraError):
     code = code_pb2.OUT_OF_RANGE
+
 
 class EtcdUnimplementedError(EtcetraError):
     code = code_pb2.UNIMPLEMENTED
 
+
 class EtcdInternalError(EtcetraError):
     code = code_pb2.INTERNAL
 
+
 class EtcdUnavailableError(EtcetraError):
     code = code_pb2.UNAVAILABLE
+
 
 class EtcdDataLossError(EtcetraError):
     code = code_pb2.DATA_LOSS
--- tests/test_error.py
+++ tests/test_error.py
@@ -3,6 +3,7 @@ import pytest
 from etcetra import EtcdClient, HostPortPair
 from etcetra.errors import EtcdUnknownError
 
+
 @pytest.mark.asyncio
 async def test_connect_error():
     etcd = EtcdClient(HostPortPair('127.0.0.1', 1))
